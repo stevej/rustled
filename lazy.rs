ee0f1ada5cececdfe45a2cbad23e5b1a4cd656f2
Steve Jenson <stevej@fruitless.org>
2012-12-06 11:32:04 -0800

    adding a basic version of Thunks

---
+++ lazy.rs
@@ -0,0 +1,51 @@
+use core::option;
+
+/**
+ * Implementation of thunks in Rust.
+ */
+pub struct Lazy<T: Copy> {
+  code          : @fn() -> T,
+  mut value     : Option<T>
+}
+
+/**
+ * Unfortunately requires a caller to make a closure.
+ */
+pure fn Lazy<T: Copy>(closure: @fn() -> T) -> Lazy<T> {
+  let l : Lazy<T> = Lazy {
+    code      : closure,
+    value     : None
+  };
+
+  return l;
+}
+
+impl<T: Copy> Lazy<T> {
+  fn force() -> T {
+    match self.value {
+      Some(value) => { return value; }
+      None => {
+        let result = self.code();
+        self.value = Some(result);
+
+        return result;
+      }
+    };
+  }
+}
+
+
+use rand::*;
+
+#[test]
+fn test_thunk() {
+
+  let thunk = Lazy (|| {
+    random()
+  });
+
+  let a1 = thunk.force();
+  let a2 = thunk.force();
+
+  assert(a1 == a2);
+}
