ffbd4483bd122b6c95669ea89bddcd2ebaa39870
Steve Jenson <stevej@fruitless.org>
2012-12-18 15:56:50 -0800

    simplify our Red-Black tree into a Left-Leaning Red-Black tree

--- red_black_tree.rs
+++ red_black_tree.rs
@@ -24,10 +24,7 @@ pure fn RBMap<K: Copy Eq Ord, V: Copy>(key: K, value: V) -> @RBMap<K, V> {
 }
 
 /**
- * A purely functional Red-Black tree.
- *
- * A port of Matt Might's Scala port of Okasaki's RB Trees: http://matt.might.net/articles/implementation-of-immutable-purely-functional-okasaki-red-black-tree-maps-in-scala/RBMap.scala
- * which was itself a port of http://www.ccs.neu.edu/course/cs3500wc/jfp99redblack.pdf
+ * A purely functional Left-Leaning Red-Black Tree.
  */
 impl<K: Copy Eq Ord, V: Copy> @RBMap<K, V> : PersistentMap<K, V> {
   pure fn get(k: K) -> @Option<V> {
@@ -57,62 +54,53 @@ impl<K: Copy Eq Ord, V: Copy> @RBMap<K, V> : PersistentMap<K, V> {
     }
   }
 
-  pure fn put(k : K, v : V) -> @RBMap<K, V> {
-    self.modifiedWith(k, @Some(v))
+  pure fn put(k: K, new_value: V) -> @RBMap<K, V> {
+    self.modify(k, @Some(new_value))
   }
 
   pure fn delete(k: K) -> @RBMap<K, V> {
-    self.modifiedWith(k, @None)
+    self.modify(k, @None)
   }
 
-  priv pure fn blacken(n : @RBMap<K, V>) -> @RBMap<K, V> {
-    match n {
-      @Leaf => n,
-      @Tree(_,l,k,v,r) => @Tree(Black,l,k,v,r)
+  priv pure fn modify(k: K, new_value: @Option<V>) -> @RBMap<K, V> {
+    match self {
+      @Leaf => @Tree(Red, self, k, new_value, self),
+      @Tree(color, left, key, original_value, right) => {
+        if (k.lt(&key)) {
+          self.balanceLeft(color, left.modify(k, new_value), key, original_value, right)
+        } else if (k == key) {
+          @Tree(color, left, k, new_value, right)
+        } else {
+          self.balanceRight(color, left, key, original_value, right.modify(k, new_value))
+        }
+      }
     }
   }
 
-  priv pure fn modifiedWith (k : K, new_value: @Option<V>) -> @RBMap<K, V> {
-    self.blacken(self.modWith(k, new_value))
-  }
-
-  priv pure fn balance (c : RBColor, l : @RBMap<K,V>, k : K, v : @Option<V>, r : @RBMap<K,V>) -> @RBMap<K,V> {
-    match (c,l,k,v,r) {
+  priv pure fn balanceLeft(c: RBColor, l: @RBMap<K,V>, k: K, v: @Option<V>, r: @RBMap<K,V>) -> @RBMap<K,V> {
+    match (c, l, k, v, r) {
       (Black,@Tree(Red,@Tree(Red,a,xK,xV,b),yK,yV,c),zK,zV,d) =>
         @Tree(Red,@Tree(Black,a,xK,xV,b),yK,yV,@Tree(Black,c,zK,zV,d)),
-
-      (Black,@Tree(Red,a,xK,xV,@Tree(Red,b,yK,yV,c)),zK,zV,d) =>
-        @Tree(Red,@Tree(Black,a,xK,xV,b),yK,yV,@Tree(Black,c,zK,zV,d)),
-
-      (Black,a,xK,xV,@Tree(Red,@Tree(Red,b,yK,yV,c),zK,zV,d)) =>
-        @Tree(Red,@Tree(Black,a,xK,xV,b),yK,yV,@Tree(Black,c,zK,zV,d)),
-
-      (Black,a,xK,xV,@Tree(Red,b,yK,yV,@Tree(Red,c,zK,zV,d))) =>
-        @Tree(Red,@Tree(Black,a,xK,xV,b),yK,yV,@Tree(Black,c,zK,zV,d)),
-
       (c,a,xK,xV,b) =>
         @Tree(c,a,xK,xV,b)
     }
   }
 
-  priv pure fn modWith (k : K, new_value: @Option<V>) -> @RBMap<K, V> {
-    match self {
-      @Leaf => @Tree(Red, self, k, new_value, self),
-      @Tree(color, left, key, original_value, right) => {
-        if (k.lt(&key)) {
-          self.balance(color, left.modWith(k, new_value), key, original_value, right)
-        } else if (k == key) {
-          @Tree(color, left, k, new_value, right)
-        } else {
-          self.balance(color, left, key, original_value, right.modWith(k, new_value))
-        }
-      }
+  priv pure fn balanceRight(c: RBColor, l: @RBMap<K,V>, k: K, v: @Option<V>, r: @RBMap<K,V>) -> @RBMap<K,V> {
+    match (c, l, k, v, r) {
+      (Black, @Tree(Red, t1, k1, v1, t2), k0, v0, @Tree(Red, t3, k2, v2, t4)) =>
+        @Tree(Red, @Tree(Black, t1, k1, v1, t2), k0, v0, @Tree(Black, t3, k2, v2, t4)),
+      (c, l, k, v, @Tree(Red, t1, k1, v1, t2)) =>
+        @Tree(c, @Tree(Red, l, k, v, t1), k1, v1, t2),
+      (c,a,xK,xV,b) =>
+        @Tree(c,a,xK,xV,b)
     }
   }
+
 }
 
 #[test]
-fn test_rb_tree_create() {
+fn test_rb_tree() {
   let v1 = RBMap("stevej", 150);
   let v2 = v1.put("thatstacy", 187);
 
@@ -124,9 +112,15 @@ fn test_rb_tree_create() {
   assert(v3.get("thatstacy") == @Some(187));
 
   let v4 = v2.put("jeremy", 16);
-  assert(v4.get("jeremy") == @Some(16));
-  assert(v4.get("stevej") == @Some(150));
-  assert(v4.get("thatstacy") == @Some(187));
+
+  let v5 = v4.put("ev", 20);
+  let v6 = v5.put("zhana", 51573);
+
+  assert(v6.get("jeremy") == @Some(16));
+  assert(v6.get("stevej") == @Some(150));
+  assert(v6.get("thatstacy") == @Some(187));
+  assert(v6.get("ev") == @Some(20));
+  assert(v6.get("zhana") == @Some(51573));
 }
 
 #[test]
