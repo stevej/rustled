29fc9d74834e8134896c15d39db2b40cf1f81132
Steve Jenson <stevej@fruitless.org>
2012-12-19 16:14:56 -0800

    killing the @box in the trait and copying the value into and out of the underlying Map

--- red_black_tree.rs
+++ red_black_tree.rs
@@ -2,7 +2,7 @@ use core::cmp::{Eq, Ord};
 use iter::BaseIter;
 
 pub trait PersistentMap<K: Copy Eq Ord, V: Copy> {
-  pure fn get(k: K) -> @Option<V>;
+  pure fn get(k: K) -> Option<V>;
   pure fn put(k: K, v: V) -> self;
   pure fn delete(k: K) -> self;
 }
@@ -14,23 +14,23 @@ enum RBColor {
 
 pub enum RBMap<K: Copy Eq Ord, V: Copy> {
   //   color    left         k  value       right
-  Tree(RBColor, @RBMap<K,V>, K, @Option<V>, @RBMap<K,V>),
+  Tree(RBColor, @RBMap<K,V>, K, Option<V>, @RBMap<K,V>),
   Leaf
 }
 
 pure fn RBMap<K: Copy Eq Ord, V: Copy>(key: K, value: V) -> @RBMap<K, V> {
-  @Tree(Red, @Leaf, key, @Some(value), @Leaf)
+  @Tree(Red, @Leaf, key, Some(value), @Leaf)
 }
 
 priv impl<K: Copy Eq Ord, V: Copy> @RBMap<K, V> {
-  pure fn modify(k: K, new_value: @Option<V>) -> @RBMap<K, V> {
+  pure fn modify(k: K, new_value: Option<V>) -> @RBMap<K, V> {
     match self {
-      @Leaf => @Tree(Red, self, k, new_value, self),
+      @Leaf => @Tree(Red, self, k, copy new_value, self),
       @Tree(color, left, key, original_value, right) => {
         if (k.lt(&key)) {
           self.balanceLeft(color, left.modify(k, new_value), key, original_value, right)
         } else if (k == key) {
-          @Tree(color, left, k, new_value, right)
+          @Tree(color, left, k, copy new_value, right)
         } else {
           self.balanceRight(color, left, key, original_value, right.modify(k, new_value))
         }
@@ -38,7 +38,7 @@ priv impl<K: Copy Eq Ord, V: Copy> @RBMap<K, V> {
     }
   }
 
-  pure fn balanceLeft(c: RBColor, l: @RBMap<K,V>, k: K, v: @Option<V>, r: @RBMap<K,V>) -> @RBMap<K,V> {
+  pure fn balanceLeft(c: RBColor, l: @RBMap<K,V>, k: K, v: Option<V>, r: @RBMap<K,V>) -> @RBMap<K,V> {
     match (c, l, k, v, r) {
       (Black,@Tree(Red,@Tree(Red,a,xK,xV,b),yK,yV,c),zK,zV,d) =>
         @Tree(Red,@Tree(Black,a,xK,xV,b),yK,yV,@Tree(Black,c,zK,zV,d)),
@@ -47,7 +47,7 @@ priv impl<K: Copy Eq Ord, V: Copy> @RBMap<K, V> {
     }
   }
 
-  pure fn balanceRight(c: RBColor, l: @RBMap<K,V>, k: K, v: @Option<V>, r: @RBMap<K,V>) -> @RBMap<K,V> {
+  pure fn balanceRight(c: RBColor, l: @RBMap<K,V>, k: K, v: Option<V>, r: @RBMap<K,V>) -> @RBMap<K,V> {
     match (c, l, k, v, r) {
       (Black, @Tree(Red, t1, k1, v1, t2), k0, v0, @Tree(Red, t3, k2, v2, t4)) =>
         @Tree(Red, @Tree(Black, t1, k1, v1, t2), k0, v0, @Tree(Black, t3, k2, v2, t4)),
@@ -64,27 +64,27 @@ priv impl<K: Copy Eq Ord, V: Copy> @RBMap<K, V> {
  * A purely functional Left-Leaning Red-Black Tree.
  */
 impl<K: Copy Eq Ord, V: Copy> @RBMap<K, V> : PersistentMap<K, V> {
-  pure fn get(k: K) -> @Option<V> {
+  pure fn get(k: K) -> Option<V> {
     match self {
-      @Leaf => @None,
+      @Leaf => None,
       @Tree(_, left, key, maybe_value, right) => {
         if (k.lt(&key)) {
           left.get(k)
         } else if (k.gt(&key)) {
           right.get(k)
         } else {
-          maybe_value
+          copy maybe_value
         }
       }
     }
   }
 
   pure fn put(k: K, new_value: V) -> @RBMap<K, V> {
-    self.modify(k, @Some(new_value))
+    self.modify(k, Some(new_value))
   }
 
   pure fn delete(k: K) -> @RBMap<K, V> {
-    self.modify(k, @None)
+    self.modify(k, None)
   }
 }
 
@@ -99,8 +99,8 @@ impl<K: Copy Eq Ord, V: Copy> RBMap<K, V>: iter::BaseIter<(&K, &V)> {
       Tree(_, left, key, maybe_value, right) => {
         left.each(f);
         match maybe_value {
-          @Some(value) => f(&(&key, &value)),
-          @None => false,
+          Some(value) => f(&(&key, &value)),
+          None => false,
         };
         right.each(f);
       }
@@ -114,23 +114,23 @@ fn test_rb_tree() {
   let v1 = RBMap("stevej", 150);
   let v2 = v1.put("thatstacy", 187);
 
-  assert(v2.get("stevej") == @Some(150));
-  assert(v2.get("thatstacy") == @Some(187));
+  assert(v2.get("stevej") == Some(150));
+  assert(v2.get("thatstacy") == Some(187));
 
   let v3 = v2.delete("stevej");
-  assert(v3.get("stevej") == @None);
-  assert(v3.get("thatstacy") == @Some(187));
+  assert(v3.get("stevej") == None);
+  assert(v3.get("thatstacy") == Some(187));
 
   let v4 = v2.put("jeremy", 16);
 
   let v5 = v4.put("ev", 20);
   let v6 = v5.put("zhana", 51573);
 
-  assert(v6.get("jeremy") == @Some(16));
-  assert(v6.get("stevej") == @Some(150));
-  assert(v6.get("thatstacy") == @Some(187));
-  assert(v6.get("ev") == @Some(20));
-  assert(v6.get("zhana") == @Some(51573));
+  assert(v6.get("jeremy") == Some(16));
+  assert(v6.get("stevej") == Some(150));
+  assert(v6.get("thatstacy") == Some(187));
+  assert(v6.get("ev") == Some(20));
+  assert(v6.get("zhana") == Some(51573));
 }
 
 #[test]
